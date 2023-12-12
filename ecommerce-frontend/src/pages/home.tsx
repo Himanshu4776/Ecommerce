e71212e2f9c876e0822552e30a3020454cedfafa
Himanshu Kumar <49893152+Himanshu4776@users.noreply.github.com>
2023-12-12 10:09:43 +0000

    Update 4 files

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import Header from "@/components/core/header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
