603ae568e6ed34499752ecfec507a54837387de4
Himanshu Kumar <49893152+Himanshu4776@users.noreply.github.com>
2023-12-15 16:37:20 +0000

    Product preview page modified

---
+++ ecommerce-frontend/src/lib/icons/icon-pc.tsx
@@ -0,0 +1,46 @@
+export function IconPc() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="-0.5 -0.5 14 14"
+      height="22"
+      width="22"
+    >
+      <g id="screen-1--screen-device-electronics-monitor-diplay-computer">
+        <path
+          id="Vector"
+          stroke="#c2410c"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12.071428571428571 1.8571428571428572H0.9285714285714286c-0.2564175714285714 0 -0.4642857142857143 0.20787 -0.4642857142857143 0.4642857142857143v7.428571428571429c0 0.25637857142857146 0.20786814285714286 0.4642857142857143 0.4642857142857143 0.4642857142857143h11.142857142857142c0.25637857142857146 0 0.4642857142857143 -0.20790714285714285 0.4642857142857143 -0.4642857142857143v-7.428571428571429c0 -0.2564157142857143 -0.20790714285714285 -0.4642857142857143 -0.4642857142857143 -0.4642857142857143Z"
+          stroke-width="1"
+        ></path>
+        <path
+          id="Vector_2"
+          stroke="#c2410c"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m5.571428571428571 10.214285714285715 -0.9285714285714286 2.3214285714285716"
+          stroke-width="1"
+        ></path>
+        <path
+          id="Vector_3"
+          stroke="#c2410c"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m7.428571428571429 10.214285714285715 0.9285714285714286 2.3214285714285716"
+          stroke-width="1"
+        ></path>
+        <path
+          id="Vector_4"
+          stroke="#c2410c"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.7142857142857144 12.535714285714286h5.571428571428571"
+          stroke-width="1"
+        ></path>
+      </g>
+    </svg>
+  );
+}
